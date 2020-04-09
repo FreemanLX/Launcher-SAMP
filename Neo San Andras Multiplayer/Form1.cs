99a0a7417ceb353b6f62b935bf9992d3e839d9f1
Andreas Mihalea <56113049+Freeman-A@users.noreply.github.com>
2020-04-09 20:14:01 +0300

    Add files via upload

---
+++ Neo San Andras Multiplayer/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+
+namespace Neo_San_Andras_Multiplayer
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Show();
+            this.Select();
+            try
+            {
+                Ping g = new Ping();
+                PingReply pg = g.Send("www.google.com");
+                if (pg.Status == IPStatus.Success)
+                {
+                    if (pg.RoundtripTime > 100)
+                    {
+                        metroLabel2.Visible = true;
+                        metroLabel2.Text = metroLabel2.Text + " Ping: " + pg.RoundtripTime.ToString() + " ms";
+                        metroLabel2.Refresh();
+                    }
+                    if (pg.RoundtripTime < 50)
+                    {
+                        metroLabel2.Visible = true;
+                        metroLabel2.Text = "You have a good internet connection";
+                        metroLabel2.Refresh();
+                    }
+
+                }
+            }
+            catch
+            {
+                try
+                {
+                    metroLabel2.Visible = true;
+                    metroLabel2.Text = metroLabel2.Text + " Ping: 9999 ms";
+                }
+                catch
+                {
+
+                }
+
+            }
+            userControl21.Show();
+            userControl11.Hide();
+            userControl31.Hide();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            userControl11.Hide();
+            userControl31.Hide();
+            userControl21.Show();
+            userControl21.BringToFront();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            userControl21.Hide();
+            userControl11.Hide();
+            userControl11.Show();
+            userControl11.BringToFront();
+        }
+
+        private void metroLabel3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void userControl21_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void userControl11_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            userControl21.Hide();
+            userControl11.Hide();
+            userControl31.Show();
+            userControl31.BringToFront();
+        }
+    }
+}
---
+++ Neo San Andras Multiplayer/Form2.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Neo_San_Andras_Multiplayer
+{
+    public partial class Form2 : MetroFramework.Forms.MetroForm
+    {
+        private readonly UserControl2 load1;
+        public Form2(UserControl2 load)
+        {
+            InitializeComponent();
+            load1 = load;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            if (metroTextBox1.Text.Length > 0)
+            {
+                string port = "";
+                if (metroTextBox2.Text.Length > 0)
+                {
+                    int ok = 0;
+                    string temp = metroTextBox2.Text;
+                    for (int i = 0; i < temp.Length; i++)
+                    {
+                        if (temp[i] < '0' || temp[i] > '9')
+                        {
+                            ok = 1;
+                            break;
+                        }
+
+                    }
+                    if (ok == 0)
+                    {
+                        MessageBox.Show("You introduced a bad port", "Bad port", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        this.Close();
+                    }
+                    else
+                    {
+                        port = metroTextBox2.Text;
+                 
+                    }
+                }
+                else
+                {
+                    port = ":7777";
+                }
+                string ip = metroTextBox1.Text;
+                string combine = ip + port;
+                string dns = "";
+                System.Net.IPAddress[] host = new System.Net.IPAddress[2];
+                try
+                {
+                    host = System.Net.Dns.GetHostAddresses(ip);
+                    foreach (System.Net.IPAddress host1 in host)
+                    {
+                        dns = host1.ToString();
+                        break;
+                    }
+                }
+                catch{}
+                string save = "@%appdata%\\NeoSAMP\\servers.db";
+                File.AppendAllText(save, combine + Environment.NewLine);
+                load1.counter(load1.getcounter() + 1);
+                load1.loading(dns);
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("You hadn't introduced any character in both text labels", "Error 001", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+            }
+        }
+    }
+}
